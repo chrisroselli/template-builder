161d9814b853cf223f9c72763c5cb346bc9935f7
chrisroselli <croselli@contractornation.com>
2024-12-17 12:07:43 -0500

    Added toast for comp create success/error

--- app/App.tsx
+++ app/App.tsx
@@ -1,83 +1,109 @@
 'use client'
-import {useState} from 'react';
-import TemplateBuilder from './components/TemplateBuilder';
-import PageBuilder from './components/PageBuilder';
-import ComponentManager from './components/ComponentManager';
-import ComponentCreator from './components/ComponentCreator';
-import {LayoutDashboard, PanelsTopLeft, PanelTop, PlusCircle} from 'lucide-react';
-import {CompRow, TemplateRow} from "@/app/types/types";
+import { useState } from 'react'
+import TemplateBuilder from './components/TemplateBuilder'
+import PageBuilder from './components/PageBuilder'
+import ComponentManager from './components/ComponentManager'
+import ComponentCreator from './components/ComponentCreator'
+import {
+  LayoutDashboard,
+  PanelsTopLeft,
+  PanelTop,
+  PlusCircle,
+} from 'lucide-react'
+import { CompRow, TemplateRow } from '@/app/types/types'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
+function App({
+  templates,
+  comps,
+}: {
+  templates: TemplateRow[]
+  comps: CompRow[]
+}) {
+  const [activeView, setActiveView] = useState<
+    'template' | 'page' | 'manager' | 'creator'
+  >('template')
 
-function App( { templates, comps }: { templates: TemplateRow[], comps: CompRow[]  } ) {
-  const [activeView, setActiveView] = useState<'template' | 'page' | 'manager' | 'creator'>('template');
   return (
-    <div className="min-h-screen bg-gray-50">
-      <nav className="bg-white shadow-sm">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="flex justify-between h-16">
-            <div className="flex">
-              <div className="flex space-x-8">
-                <button
-                  onClick={() => setActiveView('template')}
-                  className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
-                    activeView === 'template'
-                      ? 'border-primary-dark text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  }`}
-                >
-                  <PanelsTopLeft className="w-4 h-4 mr-2"/>
-                  Template Builder
-                </button>
-                <button
-                  onClick={() => setActiveView('page')}
-                  className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
-                    activeView === 'page'
-                      ? 'border-primary-dark text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  }`}
-                >
-                  <PanelTop className="w-4 h-4 mr-2"/>
-                  Page Builder
-                </button>
-                <button
-                  onClick={() => setActiveView('manager')}
-                  className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
-                    activeView === 'manager'
-                      ? 'border-primary-dark text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  }`}
-                >
-                  <LayoutDashboard className="w-4 h-4 mr-2"/>
-                  Component Manager
-                </button>
-                <button
-                  onClick={() => setActiveView('creator')}
-                  className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
-                    activeView === 'creator'
-                      ? 'border-primary-dark text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  }`}
-                >
-                  <PlusCircle className="w-4 h-4 mr-2"/>
-                  Create Component
-                </button>
+    <>
+      <div className="min-h-screen bg-gray-50">
+        <nav className="bg-white shadow-sm">
+          <div className="max-w-7xl mx-auto px-4">
+            <div className="flex justify-between h-16">
+              <div className="flex">
+                <div className="flex space-x-8">
+                  <button
+                    onClick={() => setActiveView('template')}
+                    className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+                      activeView === 'template'
+                        ? 'border-primary-dark text-gray-900'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                    }`}
+                  >
+                    <PanelsTopLeft className="w-4 h-4 mr-2" />
+                    Template Builder
+                  </button>
+                  <button
+                    onClick={() => setActiveView('page')}
+                    className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+                      activeView === 'page'
+                        ? 'border-primary-dark text-gray-900'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                    }`}
+                  >
+                    <PanelTop className="w-4 h-4 mr-2" />
+                    Page Builder
+                  </button>
+                  <button
+                    onClick={() => setActiveView('manager')}
+                    className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+                      activeView === 'manager'
+                        ? 'border-primary-dark text-gray-900'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                    }`}
+                  >
+                    <LayoutDashboard className="w-4 h-4 mr-2" />
+                    Component Manager
+                  </button>
+                  <button
+                    onClick={() => setActiveView('creator')}
+                    className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+                      activeView === 'creator'
+                        ? 'border-primary-dark text-gray-900'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                    }`}
+                  >
+                    <PlusCircle className="w-4 h-4 mr-2" />
+                    Create Component
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      {activeView === 'template' ? (
-        <TemplateBuilder templates={templates} comps={comps}/>
-      ) : activeView === 'page' ? (
-        <PageBuilder comps={comps}/>
-      ) : activeView === 'manager' ? (
-        <ComponentManager comps={comps}/>
-      ) : (
-        <ComponentCreator comps={comps}/>
-      )}
-    </div>
-  );
+        {activeView === 'template' ? (
+          <TemplateBuilder templates={templates} comps={comps} />
+        ) : activeView === 'page' ? (
+          <PageBuilder comps={comps} />
+        ) : activeView === 'manager' ? (
+          <ComponentManager comps={comps} />
+        ) : (
+          <ComponentCreator comps={comps} />
+        )}
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
--- app/components/ComponentCreator.tsx
+++ app/components/ComponentCreator.tsx
@@ -1,53 +1,43 @@
-import React, {useRef, useState, useTransition} from 'react';
-import {useRouter} from 'next/navigation';
-import {Eye, Save} from 'lucide-react';
-import type {CompRow} from '../types/types';
-import {submitComponent} from '@/app/actions/componentActions';
+import React, { useRef, useState, useTransition } from 'react'
+import { useRouter } from 'next/navigation'
+import { Eye, Save } from 'lucide-react'
+import type { CompRow } from '../types/types'
+import { submitComponent } from '@/app/actions/componentActions'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-function SuccessMessage({ message }: { message: string }) {
-  return (
-    <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-      <strong className="font-bold">Success!</strong>
-      <span className="block sm:inline"> {message}</span>
-    </div>
-  );
-}
+export default function ComponentCreator({ comps }: { comps: CompRow[] }) {
+  const [isPending, startTransition] = useTransition()
+  const [componentType, setComponentType] = useState('')
+  const [name, setName] = useState('')
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const formRef = useRef<HTMLFormElement>(null)
+  const router = useRouter()
 
-function ErrorMessage({ message, error }: { message: string; error?: string }) {
-  return (
-    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-      <strong className="font-bold">Error!</strong>
-      <span className="block sm:inline"> {message}</span>
-      {error && <p className="mt-2 text-sm">{error}</p>}
-    </div>
-  );
-}
-export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
-  const [isPending, startTransition] = useTransition();
-  const [componentType, setComponentType] = useState('');
-  const [name, setName] = useState('');
-  const [html, setHtml] = useState('');
-  const [css, setCss] = useState('');
-  const [previewVisible, setPreviewVisible] = useState(false);
-  const formRef = useRef<HTMLFormElement>(null);
-  const [message, setMessage] = useState<{ type: 'success' | 'error'; content: string; error?: string } | null>(null);
-  const router = useRouter();
-  // TODO: Revise Success and Error messages
   async function handleSubmit(formData: FormData) {
-    const result = await submitComponent(formData);
+    const result = await submitComponent(formData)
     if (result.success) {
-      setMessage({ type: 'success', content: result.message });
-    if (formRef.current) {
-      formRef.current.reset();
-      setName('');
-      setHtml('');
-      setCss('');
-    }
+      toast.success('Component submitted successfully', {
+        style: {
+          background: '#4a8332',
+        },
+      })
+      console.log(result.message)
+      if (formRef.current) {
+        formRef.current.reset()
+        setComponentType('')
+        setName('')
+        setHtml('')
+        setCss('')
+      }
       startTransition(() => {
-        router.refresh();
-      });
+        router.refresh()
+      })
     } else {
-      setMessage({ type: 'error', content: result.message, error: result.error });
+      toast.error('Component submission failed!')
+      console.log(result.message)
     }
   }
 
@@ -55,10 +45,15 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
     <div className="min-h-screen bg-gray-50 p-8">
       <div className="max-w-7xl mx-auto">
         <div className="bg-white rounded-xl shadow-lg p-6">
-          <h1 className="text-3xl font-bold text-primary mb-6">Create Component</h1>
+          <h1 className="text-3xl font-bold text-primary mb-6">
+            Create Component
+          </h1>
           <form ref={formRef} action={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="componentName" className="block text-sm font-medium text-gray-700 mb-2">
+              <label
+                htmlFor="componentName"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
                 Component Type
               </label>
               <select
@@ -70,13 +65,22 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
                 value={componentType}
               >
                 <option value="">Select Component Type</option>
-                {[...new Set(comps.map(({ comp_type }) => comp_type))].map((comp_type, id) => (
-                  <option key={id} value={comp_type}>{comp_type === 'Services' ? comp_type : comp_type.slice(0, -1)}</option>
-                ))}
+                {[...new Set(comps.map(({ comp_type }) => comp_type))].map(
+                  (comp_type, id) => (
+                    <option key={id} value={comp_type}>
+                      {comp_type === 'Services'
+                        ? comp_type
+                        : comp_type.slice(0, -1)}
+                    </option>
+                  ),
+                )}
               </select>
             </div>
             <div>
-              <label htmlFor="componentName" className="block text-sm font-medium text-gray-700 mb-2">
+              <label
+                htmlFor="componentName"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
                 Component Name
               </label>
               <input
@@ -91,7 +95,10 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
               />
             </div>
             <div>
-              <label htmlFor="html" className="block text-sm font-medium text-gray-700 mb-2">
+              <label
+                htmlFor="html"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
                 HTML
               </label>
               <textarea
@@ -106,7 +113,10 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
               />
             </div>
             <div>
-              <label htmlFor="css" className="block text-sm font-medium text-gray-700 mb-2">
+              <label
+                htmlFor="css"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
                 CSS
               </label>
               <textarea
@@ -121,33 +131,41 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
               />
             </div>
             <div className="flex gap-2">
-                  <button
-                    type="button"
-                    disabled={componentType === '' || name === '' || html === '' || css === ''}
-                    onClick={() => setPreviewVisible(!previewVisible)}
-                    className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 cursor-pointer"
-                  >
-                    <Eye className="w-4 h-4 mr-2"/>
-                    {previewVisible ? 'Hide Preview' : 'Show Preview'}
-                  </button>
+              <button
+                type="button"
+                disabled={
+                  componentType === '' ||
+                  name === '' ||
+                  html === '' ||
+                  css === ''
+                }
+                onClick={() => setPreviewVisible(!previewVisible)}
+                className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 cursor-pointer"
+              >
+                <Eye className="w-4 h-4 mr-2" />
+                {previewVisible ? 'Hide Preview' : 'Show Preview'}
+              </button>
               <button
                 type="submit"
-                disabled={isPending || componentType === '' || name === '' || html === '' || css === ''}
+                disabled={
+                  isPending ||
+                  componentType === '' ||
+                  name === '' ||
+                  html === '' ||
+                  css === ''
+                }
                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-dark hover:bg-primary-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-dark disabled:opacity-50 cursor-pointer"
               >
                 <Save className="w-4 h-4 mr-2" />
                 {isPending ? 'Submitting...' : 'Submit Component'}
               </button>
             </div>
-            {message && (
-              message.type === 'success'
-                ? <SuccessMessage message={message.content} />
-                : <ErrorMessage message={message.content} error={message.error} />
-            )}
           </form>
           {previewVisible && (
             <div className="mt-8">
-              <h2 className="text-lg font-medium text-gray-900 mb-4">Preview</h2>
+              <h2 className="text-lg font-medium text-gray-900 mb-4">
+                Preview
+              </h2>
               <div className="border rounded-lg overflow-hidden">
                 <style>{css}</style>
                 <div dangerouslySetInnerHTML={{ __html: html }} />
@@ -157,5 +175,5 @@ export default function ComponentCreator({ comps }: { comps: CompRow[]} ) {
         </div>
       </div>
     </div>
-  );
+  )
 }
